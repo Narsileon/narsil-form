1a7843d639bc5ba172c1722f6efa5f8b1a047553
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-08 14:58:11 +0200

    Extend submit parameters types

--- resources/js/Components/Form/form.d.ts
+++ resources/js/Components/Form/form.d.ts
@@ -1,9 +1,13 @@
+type SubmitParametersType = {
+	preserveScroll?: boolean;
+	preserveState?: boolean;
+	onError?: (errors: Record<string, string>) => void;
+	onSuccess?: () => void;
+};
+
 interface FormProps extends React.HTMLAttributes<HTMLFormElement> {
 	className?: string;
 	method?: "patch" | "post";
 	route: string;
-	submitParameters?: {
-		onError?: (errors: Record<string, string>) => void;
-		onSuccess?: () => void;
-	};
+	submitParameters?: SubmitParametersType;
 }
