dabc948abb5156f794f9eec38fedb5d7d0082570
Narsilien <rigaux.jonathan@gmail.com>
2024-08-09 05:30:56 +0200

    Add missing export

--- resources/js/Components/Form/index.ts
+++ resources/js/Components/Form/index.ts
@@ -6,6 +6,18 @@ import FormItem from "./FormItem";
 import FormLabel from "./FormLabel";
 import FormMessage from "./FormMessage";
 import FormProvider from "./FormProvider";
+import FormRenderer from "./FormRenderer";
 import useForm from "./useForm";
 
-export { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, FormProvider, useForm };
+export {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	FormProvider,
+	FormRenderer,
+	useForm,
+};
