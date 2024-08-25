81716d9a978e0669e7911c24eba77764ccf01ad9
Narsilien <rigaux.jonathan@gmail.com>
2024-08-25 18:45:13 +0200

    Readd focus ring

--- resources/js/Components/Textarea/Textarea.tsx
+++ resources/js/Components/Textarea/Textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
 		className={cn(
 			"border-input bg-background ring-offset-background flex min-h-[80px] w-full rounded-md border px-3 py-2 text-sm",
 			"placeholder:text-muted-foreground",
-			"focus-visible:border-primary focus-visible:outline-none",
+			"focus-visible:ring-primary-highlight focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
 			"disabled:cursor-not-allowed disabled:opacity-50",
 			className
 		)}
