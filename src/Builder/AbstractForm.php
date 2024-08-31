a433fe503303c206e1d94252db72622f37859722
Narsilien <rigaux.jonathan@gmail.com>
2024-08-31 16:10:06 +0200

    Split abstract form in service

--- src/Builder/AbstractForm.php
+++ src/Builder/AbstractForm.php
@@ -7,10 +7,8 @@ namespace Narsil\Forms\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Str;
-use Narsil\Forms\Models\Form;
-use Narsil\Forms\Models\FormNode;
-use Narsil\Forms\Models\FormNodeOption;
 use Narsil\Forms\Http\Resources\FormResource;
+use Narsil\Forms\Services\FormService;
 
 #endregion
 
@@ -56,15 +54,29 @@ abstract class AbstractForm extends JsonResource
     #region PUBLIC METHODS
 
     /**
+     * @return FormResource
+     */
+    public function getForm(): FormResource
+    {
+        $form = FormService::getForm(
+            nodes: $this->getSchema(),
+            slug: $this->slug,
+            title: $this->title,
+        );
+
+        $form = new FormResource($form, $this->getOptions());
+
+        return $form;
+    }
+
+    /**
      * @param Request $request
      *
      * @return array
      */
     public function toArray(Request $request): array
     {
-        $attributes = [];
-
-        $attributes = $this->resource?->toArray();
+        $attributes = $this->resource ? $this->resource->toArray() : [];
 
         return $attributes;
     }
@@ -76,7 +88,7 @@ abstract class AbstractForm extends JsonResource
      */
     public function with($request): array
     {
-        $form = new FormResource($this->getForm(), $this->getOptions());;
+        $form = $this->getForm();
         $meta = $this->getMeta();
         $slug = $this->getSlug();
 
@@ -117,97 +129,11 @@ abstract class AbstractForm extends JsonResource
     #region PRIVATE METHODS
 
     /**
-     * @param array<AbstractFormNode> $nodes
-     *
-     * @return Form
+     * @return string|null
      */
-    private function createForm(array $nodes): Form
+    private function getSlug(): string|null
     {
-        $form = Form::firstOrCreate([
-            Form::SLUG => $this->slug,
-        ], [
-            Form::TITLE => $this->title,
-        ]);
-
-        $this->createFormNodesRecursively($form, $nodes);
-
-        return $form;
-    }
-
-    /**
-     * @param Form $form
-     * @param array<AbstractFormNode> $nodes
-     * @param FormNode $parentNode
-     *
-     * @return void
-     */
-    private function createFormNodesRecursively(Form $form, array $nodes, FormNode $parentNode = null): void
-    {
-        foreach ($nodes as $node)
-        {
-            $node = $node->get();
-
-            $formNode = FormNode::firstOrCreate([
-                FormNode::FORM_ID => $form->{Form::ID},
-                FormNode::IDENTIFIER => $node[FormNode::IDENTIFIER] ?? null,
-                FormNode::NODE_TYPE => $node[FormNode::NODE_TYPE],
-                FormNode::PARENT_ID => $parentNode?->{FormNode::ID},
-            ], array_filter([
-                FormNode::AUTO_COMPLETE => $node[FormNode::AUTO_COMPLETE] ?? null,
-                FormNode::DEFAULT_VALUE => $node[FormNode::DEFAULT_VALUE] ?? null,
-                FormNode::DESCRIPTION => $node[FormNode::DESCRIPTION] ?? null,
-                FormNode::LABEL => $node[FormNode::LABEL] ?? null,
-                FormNode::MAX => $node[FormNode::MAX] ?? null,
-                FormNode::MIN => $node[FormNode::MIN] ?? null,
-                FormNode::PARAMETERS => $node[FormNode::PARAMETERS] ?? null,
-                FormNode::PLACEHOLDER => $node[FormNode::PLACEHOLDER] ?? null,
-                FormNode::TYPE => $node[FormNode::TYPE] ?? null,
-                FormNode::REQUIRED => $node[FormNode::REQUIRED] ?? false,
-            ]));
-
-            if ($options = $node[FormNode::RELATIONSHIP_OPTIONS] ?? null)
-            {
-                foreach ($options as $option)
-                {
-                    FormNodeOption::firstOrCreate([
-                        FormNodeOption::NODE_ID => $formNode->{FormNode::ID},
-                        FormNodeOption::VALUE => $option[FormNodeOption::VALUE] ?? $option,
-                    ], [
-                        FormNodeOption::LABEL => $option[FormNodeOption::LABEL] ?? $option,
-                    ]);
-                }
-            }
-
-            if ($children = $node[FormNode::RELATIONSHIP_CHILDREN] ?? null)
-            {
-                $this->createFormNodesRecursively($form, $children, $formNode);
-            }
-        }
-    }
-
-    /**
-     * @return Form
-     */
-    private function getForm(): Form
-    {
-        $form = Form::firstWhere(Form::SLUG);
-
-        if (!$form)
-        {
-            $schema = $this->getSchema();
-
-            $form = $this->createForm($schema);
-        }
-
-        return $form;
-    }
-
-    /**
-     * @return string
-     */
-    private function getSlug(): string
-    {
-        return Str::slug($this->resource->getTable());
+        return $this->resource ? Str::slug($this->resource->getTable()) : null;
     }
 
     #endregion
---
+++ src/Services/FormService.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Narsil\Forms\Services;
+
+#region USE
+
+use Narsil\Forms\Models\Form;
+use Narsil\Forms\Models\FormNode;
+use Narsil\Forms\Models\FormNodeOption;
+
+#endregion
+
+/**
+ * @version 1.0.0
+ *
+ * @author Jonathan Rigaux
+ */
+abstract class FormService
+{
+    #region PUBLIC METHODS
+
+    /**
+     * @param string $title
+     * @param string $slug
+     * @param array<AbstractFormNode> $nodes
+     *
+     * @return Form
+     */
+    public static function getForm(string $title, string $slug, array $nodes): Form
+    {
+        $form = Form::firstWhere([
+            Form::SLUG => $slug,
+        ]);
+
+        if (!$form)
+        {
+            $form = static::createForm($title, $slug, $nodes);
+        }
+
+        return $form;
+    }
+
+    #endregion
+
+    #region PRIVATE METHODS
+
+    /**
+     * @param string $title
+     * @param string $slug
+     * @param array<AbstractFormNode> $nodes
+     *
+     * @return Form
+     */
+    private static function createForm(string $title, string $slug, array $nodes): Form
+    {
+        $form = Form::firstOrCreate([
+            Form::SLUG => $slug,
+        ], [
+            Form::TITLE => $title,
+        ]);
+
+        static::createFormNodesRecursively($form, $nodes);
+
+        return $form;
+    }
+
+    /**
+     * @param Form $form
+     * @param array<AbstractFormNode> $nodes
+     * @param FormNode $parentNode
+     *
+     * @return void
+     */
+    private static function createFormNodesRecursively(Form $form, array $nodes, FormNode $parentNode = null): void
+    {
+        foreach ($nodes as $node)
+        {
+            $node = $node->get();
+
+            $formNode = FormNode::firstOrCreate([
+                FormNode::FORM_ID => $form->{Form::ID},
+                FormNode::IDENTIFIER => $node[FormNode::IDENTIFIER] ?? null,
+                FormNode::NODE_TYPE => $node[FormNode::NODE_TYPE],
+                FormNode::PARENT_ID => $parentNode?->{FormNode::ID},
+            ], array_filter([
+                FormNode::AUTO_COMPLETE => $node[FormNode::AUTO_COMPLETE] ?? null,
+                FormNode::DEFAULT_VALUE => $node[FormNode::DEFAULT_VALUE] ?? null,
+                FormNode::DESCRIPTION => $node[FormNode::DESCRIPTION] ?? null,
+                FormNode::LABEL => $node[FormNode::LABEL] ?? null,
+                FormNode::MAX => $node[FormNode::MAX] ?? null,
+                FormNode::MIN => $node[FormNode::MIN] ?? null,
+                FormNode::PARAMETERS => $node[FormNode::PARAMETERS] ?? null,
+                FormNode::PLACEHOLDER => $node[FormNode::PLACEHOLDER] ?? null,
+                FormNode::TYPE => $node[FormNode::TYPE] ?? null,
+                FormNode::REQUIRED => $node[FormNode::REQUIRED] ?? false,
+            ]));
+
+            if ($options = $node[FormNode::RELATIONSHIP_OPTIONS] ?? null)
+            {
+                foreach ($options as $option)
+                {
+                    FormNodeOption::firstOrCreate([
+                        FormNodeOption::NODE_ID => $formNode->{FormNode::ID},
+                        FormNodeOption::VALUE => $option[FormNodeOption::VALUE] ?? $option,
+                    ], [
+                        FormNodeOption::LABEL => $option[FormNodeOption::LABEL] ?? $option,
+                    ]);
+                }
+            }
+
+            if ($children = $node[FormNode::RELATIONSHIP_CHILDREN] ?? null)
+            {
+                static::createFormNodesRecursively($form, $children, $formNode);
+            }
+        }
+    }
+
+    #endregion
+}
