7a395236e9376f1cfdfaf08f5a7222d316a43a9b
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-09-04 14:42:31 +0200

    Make default value nullable

--- resources/js/Components/Form/formSchemas.ts
+++ resources/js/Components/Form/formSchemas.ts
@@ -3,7 +3,7 @@ import { z, ZodOptional, ZodString } from "zod";
 
 export const transSchema = (languages: LanguageModel[]) => {
 	return z.object({
-		default_value: z.string().optional(),
+		default_value: z.string().nullable(),
 		id: z.number().optional(),
 		values: z.object(
 			languages.reduce(
