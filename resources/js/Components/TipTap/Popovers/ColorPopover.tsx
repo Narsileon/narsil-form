df530b2602443682091fe7cc207d1b2a0c8ad3e3
Narsilien <rigaux.jonathan@gmail.com>
2024-09-10 19:10:06 +0200

    Add button to reset color and highlight

--- resources/js/Components/TipTap/Popovers/ColorPopover.tsx
+++ resources/js/Components/TipTap/Popovers/ColorPopover.tsx
@@ -7,6 +7,7 @@ import Button from "@narsil-ui/Components/Button/Button";
 import Popover from "@narsil-ui/Components/Popover/Popover";
 import PopoverContent from "@narsil-ui/Components/Popover/PopoverContent";
 import PopoverTrigger from "@narsil-ui/Components/Popover/PopoverTrigger";
+import Separator from "@narsil-ui/Components/Separator/Separator";
 import TooltipWrapper from "@narsil-ui/Components/Tooltip/TooltipWrapper";
 import type { PopoverTriggerProps } from "@narsil-ui/Components/Popover/PopoverTrigger";
 
@@ -26,8 +27,8 @@ const ColorPopover = React.forwardRef<HTMLButtonElement, ColorPopoverProps>(({ e
 	return (
 		<Popover>
 			<TooltipWrapper tooltip={textColorLabel}>
-                <PopoverTrigger
-                    ref={ref}
+				<PopoverTrigger
+					ref={ref}
 					asChild={true}
 					{...props}
 				>
@@ -45,29 +46,38 @@ const ColorPopover = React.forwardRef<HTMLButtonElement, ColorPopoverProps>(({ e
 				</PopoverTrigger>
 			</TooltipWrapper>
 
-			<PopoverContent className='grid w-fit grid-cols-4'>
-				{Object.entries(inputColors).map(([colorName, colorValue]) => {
-					const colorLabel = trans(`colors.${colorName}`);
+			<PopoverContent className='w-fit space-y-2 p-2'>
+				<div className='grid w-fit grid-cols-4'>
+					{Object.entries(inputColors).map(([colorName, colorValue]) => {
+						const colorLabel = trans(`colors.${colorName}`);
 
-					return (
-						<TooltipWrapper
-							tooltip={colorLabel}
-							key={colorName}
-						>
-							<Button
-								aria-label={colorLabel}
-								size='icon'
-								variant='ghost'
-								onClick={() => editor.chain().focus().setColor(colorValue).run()}
+						return (
+							<TooltipWrapper
+								tooltip={colorLabel}
+								key={colorName}
 							>
-								<div
-									className='h-6 w-6 rounded'
-									style={{ backgroundColor: colorValue }}
-								/>
-							</Button>
-						</TooltipWrapper>
-					);
-				})}
+								<Button
+									aria-label={colorLabel}
+									size='icon'
+									variant='ghost'
+									onClick={() => editor.chain().focus().setColor(colorValue).run()}
+								>
+									<div
+										className='h-6 w-6 rounded'
+										style={{ backgroundColor: colorValue }}
+									/>
+								</Button>
+							</TooltipWrapper>
+						);
+					})}
+				</div>
+				<Separator />
+				<Button
+					className='w-full'
+					onClick={() => editor.chain().focus().unsetColor()}
+				>
+					{trans("Clear")}
+				</Button>
 			</PopoverContent>
 		</Popover>
 	);
